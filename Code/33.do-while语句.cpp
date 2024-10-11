240266ae802971fb8ab897d323b9892eb910caca
xuzhaokang <3287554459@qq.com>
2024-10-11 16:23:43 +0800

    10/11 16:24

---
+++ Code/33.do-while语句.cpp
@@ -0,0 +1,16 @@
+#include <iostream>
+using namespace std;
+int main()
+{
+    int num = 0;
+    //先运行do内的操作，在经过while判断决定是否继续循环
+    do
+    {
+        cout << num << endl;
+        num ++;
+        
+    } while (num < 10);
+    
+    
+    return 0;
+}
