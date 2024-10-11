c7f23263c47a1c3472db1ebea1b3b38bda5aff18
xuzhaokang <3287554459@qq.com>
2024-10-11 20:38:01 +0800

    10/11 20:38

---
+++ Code/38.乘法口诀表.cpp
@@ -0,0 +1,16 @@
+#include <iostream>
+using namespace std;
+int main()
+{
+    for (int h = 1; h <= 9; h++)
+    {
+        for (int l = 1; l <= h; l++) //列数小于等于行数
+        {
+            cout << l << " * " << h << " = " << l * h << "\t";
+        }
+        cout << endl;
+    }
+    
+    
+    return 0;
+}
---
+++ Code/39.跳转语句.cpp
@@ -0,0 +1,73 @@
+#include <iostream>
+using namespace std;
+int main()
+{
+    //出现在Switch条件语句中，作用是终止case并跳出switch
+
+    /*  
+
+    int val = 0;
+
+    cout << "请选择副本难度：" << endl;
+    cout << "1.简单" << endl;
+    cout << "2.普通" << endl;
+    cout << "3.困难" << endl;
+
+    cin >> val;
+
+    switch (val)
+    {
+    case 1:
+        cout << "您选择的是简单难度。" << endl;
+        break;
+    case 2:
+        cout << "您选择的是普通难度。" << endl;
+        break;
+    case 3:
+        cout << "您选择的是困难难度。" << endl;
+        break;        
+    default:
+        break;
+    } 
+    
+    */
+
+    
+    //出现在循环语句中，作用是跳出当前的循环语句
+
+    /*  
+
+    for (int i = 0; i < 10; i++)
+    {
+        if (i == 5)
+        {
+            break;
+        }
+
+        cout << i << endl;
+    }
+
+    */
+
+    //出现在嵌套循环中，跳出最近的内层循环语句
+
+    for (int i = 0; i < 10; i++)
+    {
+        for (int j = 0; j < 10; j++)
+        {
+            if (j == 5)
+            {
+                break;
+            }
+            
+            cout << "* ";
+        }
+        cout << endl;
+        
+    }
+    
+    
+
+
+    return 0;
+}
