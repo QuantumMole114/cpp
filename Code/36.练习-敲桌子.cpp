46b8b059076be064d7a0f0467483424a77bf8740
xuzhaokang <3287554459@qq.com>
2024-10-11 19:30:53 +0800

    10/11 19:31

---
+++ Code/36.练习-敲桌子.cpp
@@ -0,0 +1,29 @@
+#include <iostream>
+using namespace std;
+int main()
+{
+    int num = 0;
+
+    do
+    {
+        num ++;
+
+        int a = num / 10;
+        int b = num % 10;
+
+        if (a == 7 || b == 7 || num % 7 == 0)
+        {
+            cout << "敲桌子" << endl;
+        }
+        
+        else
+        {
+            cout << num << endl;
+        }
+        
+        
+    } while (num < 100);
+    
+    
+    return 0;
+}
