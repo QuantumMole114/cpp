d5928130512d561646bf2e1b6251fbc6271edd5a
xuzhaokang <3287554459@qq.com>
2024-12-01 15:50:10 +0800

    regular

--- cpp_code/108.拷贝构造函数调用时机.cpp
+++ cpp_code/108.拷贝构造函数调用时机.cpp
@@ -38,11 +38,34 @@ void test01()
 }
 
 // 2.值传递的方式给函数参数传值
+void doWork(Person p) // 值传递 调用拷贝构造函数 这也就解释了为什么值传递不改变实参
+{
+}
+
+void test02()
+{
+    Person p;
+    doWork(p);
+}
+
 // 3.值方式返回局部对象
+Person doWork02()
+{
+    Person p1;
+    return p1;
+}
+void test03()
+{
+    Person p = doWork02();
+}
+// 此处理论上会调用拷贝构造函数，但编译器对返回值进行优化(RVO 是一种编译器优化技术。编译器会在可能的情况下直接构造返回值到调用者的目标内存位置，而不是构造一个临时对象再拷贝它。)
+// 因此输出中并没有拷贝构造函数的输出
 
 int main()
 {
-    test01();
+    // test01();
+    // test02();
+    test03();
 
     return 0;
 }
